30af94866a257dbf6dac2d8980007d8fd310fd3e
nmarth2993 <nmarth2993@gmail.com>
2019-11-06 21:13:48 -0500

    MUCH cleaner code
    like, so much cleaner that you shouldn't even look at the previous commit for your own sake

--- Movement.java
+++ Movement.java
@@ -1,207 +1,75 @@
-import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.util.Random;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
 public class Movement {
-	
+
 	JFrame frame;
 	DrawPanel panel;
-	
-	Random r = new Random();
-	
-	final int width = 30;
-	final int height = 30;
+
+	final static int DIAMETER = 25;
 	final int delta = 1;
 	final int timing = 5;
-	
-	final int DOT_SIZE = 29;
-	
-	final int pixels = 900;
-	int dots = 3;
-	
-	int[] xPos = new int[pixels];
-	int[] yPos = new int[pixels];
-	
-	int x = 0;
-	int y = 0;
-	
+
+	int x;
+	int y;
+
 	public Movement() {
-//		for (int z = 0; z < dots; z++) {
-//            xPos[z] = 50 - z * DOT_SIZE;
-//            yPos[z] = 50;
-//        }
+		x = y = 0;
+
 		frame = new JFrame();
 		panel = new DrawPanel();
-		
+
 		frame.addKeyListener(new KeyListen());
-		
+
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setContentPane(panel);
 		frame.setVisible(true);
 		panel.setPreferredSize(new Dimension(300, 300));
 		frame.pack();
-		
-		new Thread() {
-			public void run() {
-				for(;;) {
+	}
+
+	class KeyListen implements KeyListener {
+
+		boolean upPressed;
+		boolean downPressed;
+		boolean leftPressed;
+		boolean rightPressed;
+
+		public KeyListen() {
+			upPressed = downPressed = leftPressed = rightPressed = false;
+			new Thread(() -> {
+				for (;;) {
+					if (upPressed) {
+						y -= delta;
+					}
+					if (downPressed) {
+						y += delta;
+					}
+					if (leftPressed) {
+						x -= delta;
+					}
+					if (rightPressed) {
+						x += delta;
+					}
 					try {
-						Thread.sleep(10);
+						Thread.sleep(timing);
 					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
 					panel.repaint();
 				}
-			}
-		}.start();
-	}
-	
-	public static void main(String[] args) {
-		SwingUtilities.invokeLater(new Runnable() {
-			public void run() {
-				JFrame.setDefaultLookAndFeelDecorated(true);
-				new Movement();
-			}
-		});
-	}
-	
-	class KeyListen implements KeyListener {
-
-		boolean upPressed = false;
-		boolean downPressed = false;
-		boolean leftPressed = false;
-		boolean rightPressed = false;
-		
-		public KeyListen() {
-			moveListener();
-			new Thread() {
-				public void run() {
-					for (;;) {
-						while (upPressed) {
-							y -= delta;
-							try {
-								Thread.sleep(timing);
-							} catch (InterruptedException e) {
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-							}
-						}
-						try {
-							Thread.sleep(1);
-						} catch (InterruptedException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
-					}
-				}
-			}.start();
-			new Thread() {
-				public void run() {
-					for (;;) {
-						while (downPressed) {
-							y += delta;
-							try {
-								Thread.sleep(timing);
-							} catch (InterruptedException e) {
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-							}
-						}
-						try {
-							Thread.sleep(1);
-						} catch (InterruptedException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
-					}
-				}
-			}.start();
-			new Thread() {
-				public void run() {
-					for (;;) {
-						while (leftPressed) {
-							x -= delta;
-							try {
-								Thread.sleep(timing);
-							} catch (InterruptedException e) {
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-							}
-						}
-						try {
-							Thread.sleep(1);
-						} catch (InterruptedException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
-					}
-				}
-			}.start();
-			new Thread() {
-				public void run() {
-					for (;;) {
-						while (rightPressed) {
-							x += delta;
-							try {
-								Thread.sleep(timing);
-							} catch (InterruptedException e) {
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-							}
-						
-						}
-						try {
-							Thread.sleep(1);
-						} catch (InterruptedException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
-					}
-				}
-			}.start();
+			}).start();
 		}
-		
-		
-		public void moveListener() {
-			new Thread() {
-				public void run() {
-					for (;;) {
-						try {
-							Thread.sleep(1);
-						} catch (InterruptedException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
-						if (leftPressed) {
-				            xPos[0] -= DOT_SIZE;
-				        }
 
-				        if (rightPressed) {
-				            xPos[0] += DOT_SIZE;
-				        }
-
-				        if (upPressed) {
-				            yPos[0] -= DOT_SIZE;
-				        }
-
-				        if (downPressed) {
-				            yPos[0] += DOT_SIZE;
-				        }
-					}
-				}
-			}.start();
-		}
-		
 		@Override
 		public void keyPressed(KeyEvent e) {
-			// TODO Auto-generated method stub
+
 			if (e.getKeyCode() == KeyEvent.VK_UP) {
 				upPressed = true;
 			}
@@ -218,7 +86,7 @@ public class Movement {
 
 		@Override
 		public void keyReleased(KeyEvent e) {
-			// TODO Auto-generated method stub
+
 			if (e.getKeyCode() == KeyEvent.VK_UP) {
 				upPressed = false;
 			}
@@ -235,16 +103,24 @@ public class Movement {
 
 		@Override
 		public void keyTyped(KeyEvent e) {
-			// TODO Auto-generated method stub
 			keyPressed(e);
 		}
-		
+
 	}
-	
+
 	class DrawPanel extends JPanel {
 		public void paintComponent(Graphics g) {
 			super.paintComponent(g);
-			g.fillOval(x, y, width, height);
+			g.fillOval(x, y, DIAMETER, DIAMETER);
 		}
+	}
+
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				JFrame.setDefaultLookAndFeelDecorated(true);
+				new Movement();
+			}
+		});
 	}
 }
